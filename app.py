02149975637f6912c69bdeef310e83de1835e14f
Alexander Ryabyy <ryabyy.alexander@gmail.com>
2023-11-03 06:04:33 +0200

    refactoring app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-if __name__ == "__main__":
-    from data import bot
+import asyncio
+
+
+async def main():
     from data import dp
     from handlers import user_blocked_bot, photo, echo
+    from data import bot
 
     photo(dp)
     user_blocked_bot(dp)
     echo(dp)
 
-    dp.run_polling(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
