b11eefead2e0896be8ed61c32f01af5d146a5707
Oleksandr Riabyi <ryabyy.alexander@gmail.com>
2023-11-05 23:42:12 +0200

    refactoring edit media template

--- handlers/id_media.py
+++ handlers/id_media.py
@@ -10,22 +10,31 @@ audios: list[str] = []
 voices: list[str] = []
 documents: list[str] = []
 stickers: list[str] = []
+animations: list[str] = []
 
 
-@router.message(F.photo | F.video | F.sticker | F.document | F.voice | F.audio)
+@router.message(F.photo | F.video | F.sticker | F.document | F.voice | F.audio | F.animation)
 async def set_media(message: Message):
     ct = message.content_type
     if ct == 'photo':
         photos.append(message.photo[-1].file_id)
+        print(*photos)
     elif ct == 'sticker':
         stickers.append(message.sticker.file_id)
+        print(*stickers)
     elif ct == 'document':
         documents.append(message.document.file_id)
+        print(*documents)
     elif ct == 'voice':
         voices.append(message.voice.file_id)
+        print(*voices)
     elif ct == 'audio':
         audios.append(message.audio.file_id)
+        print(*audios)
     elif ct == 'video':
         videos.append(message.video.file_id)
-        print(videos)
+        print(*videos)
+    elif ct == 'animation':
+        animations.append(message.animation.file_id)
+        print(*animations)
     await message.delete()
--- handlers/slide_media.py
+++ handlers/slide_media.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import (CallbackQuery, InputMediaAudio,
-                           InputMediaDocument, InputMediaPhoto,
+                           InputMediaAnimation, InputMediaDocument, InputMediaPhoto,
                            InputMediaVideo, Message)
 from aiogram.exceptions import TelegramBadRequest
 
@@ -10,24 +10,26 @@ from keyboards import create_inline_kb
 router = Router()
 v1 = 'BAACAgIAAxkBAAIcGmVHiZq-AreHSC6VmtUR8NNiVmzYAAJpNAAC_4hBSn0eQv-9BjlFMwQ'
 v2 = 'BAACAgIAAxkBAAIcG2VHiag3YYdWVqeRIpmsZX1X357TAAJtNAAC_4hBSuHwaSeSuRKJMwQ'
-
+d1 = 'BQACAgIAAxkBAAIcJGVH_Ssy6A_K6oNYR666E0W0QyvbAAJROQAC_4hBSpn-RRcwW_BzMwQ'
+d2 = 'BQACAgIAAxkBAAIcJWVH_TaerIy6bi9SrbBOdqVkvD8WAAJSOQAC_4hBSl67emTu27U3MwQ'
+a1 = 'CQACAgIAAxkBAAIcYWVIC6jzvuKFEwVlcZZF_jnk-7D5AALqOQAC_4hBSgeX32oF4oc0MwQ'
+a2 = 'CQACAgIAAxkBAAIcJ2VH_bbpE8cAARDMDQvlIJJHqa4cBAACVjkAAv-IQUq03ry-9QABA04zBA'
+ph1 = 'AgACAgIAAxkBAAIcKmVH_usfG2a0fAz5hCCnKvygRN-CAAKU0zEb_4hBSomVhnlDI70QAQADAgADeQADMwQ'
+ph2 = 'AgACAgIAAxkBAAIcK2VH_wpN3q2j6ylFbjReNCmLYrmbAAKV0zEb_4hBSspkZIYseXqIAQADAgADeQADMwQ'
+g1 = 'CgACAgIAAxkBAAIcRGVIB3yfF8fmuX0R56VVHgkE3DqHAAI3NAACf8XRSbuAV4YGegc2MwQ'
+g2 = 'CgACAgIAAxkBAAIcRWVIB4MXhwj8KA8D4TOQlP3U_bNoAAJHMQACRWX5SpLiGvQ9pzK7MwQ'
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@router.message(F.text == 'v')
+@router.message(F.text == 'vv')
 async def process_sl(message: Message):
     markup = create_inline_kb(2, 'video')
     await message.answer_video(video=v1,
-                               caption='Это медиа 1',
+                               caption='Это video 1',
                                reply_markup=markup)
 
 
 # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-@router.callback_query(F.data.in_(['text',
-                                   'audio',
-                                   'video',
-                                   'document',
-                                   'photo',
-                                   'voice']))
+@router.callback_query(F.data.in_(['video']))
 async def process_button_press(callback: CallbackQuery, bot: bot):
     markup = create_inline_kb(2, 'video')
     try:
@@ -36,7 +38,7 @@ async def process_button_press(callback: CallbackQuery, bot: bot):
             message_id=callback.message.message_id,
             media=InputMediaVideo(
                 media=v2,
-                caption='Это медиа 2'),
+                caption='Это video 2'),
             reply_markup=markup)
     except TelegramBadRequest:
         await bot.edit_message_media(
@@ -44,5 +46,122 @@ async def process_button_press(callback: CallbackQuery, bot: bot):
             message_id=callback.message.message_id,
             media=InputMediaVideo(
                 media=v1,
-                caption='Это медиа 1'),
+                caption='Это video 1'),
+            reply_markup=markup)
+
+@router.message(F.text == 'ff')
+async def process_sl(message: Message):
+    markup = create_inline_kb(2, 'photo')
+    await message.answer_photo(photo=ph1,
+                               caption='Это photo 1',
+                               reply_markup=markup)
+
+
+# Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
+@router.callback_query(F.data.in_(['photo']))
+async def process_button_press(callback: CallbackQuery, bot: bot):
+    markup = create_inline_kb(2, 'photo')
+    try:
+        await bot.edit_message_media(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            media=InputMediaPhoto(
+                media=ph2,
+                caption='Это photo 2'),
+            reply_markup=markup)
+    except TelegramBadRequest:
+        await bot.edit_message_media(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            media=InputMediaPhoto(
+                media=ph1,
+                caption='Это photo 1'),
+            reply_markup=markup)
+
+
+@router.message(F.text == 'aa')
+async def process_sl(message: Message):
+    markup = create_inline_kb(2, 'audio')
+    await message.answer_audio(audio=a1,
+                               caption='Это audio 1',
+                               reply_markup=markup)
+
+
+# Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
+@router.callback_query(F.data.in_(['audio']))
+async def process_button_press(callback: CallbackQuery, bot: bot):
+    markup = create_inline_kb(2, 'audio')
+    try:
+        await bot.edit_message_media(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            media=InputMediaAudio(
+                media=a2,
+                caption='Это audio 2'),
+            reply_markup=markup)
+    except TelegramBadRequest:
+        await bot.edit_message_media(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            media=InputMediaAudio(
+                media=a1,
+                caption='Это audio 1'),
+            reply_markup=markup)
+
+@router.message(F.text == 'gg')
+async def process_sl(message: Message):
+    markup = create_inline_kb(2, 'animation')
+    await message.answer_animation(animation=g1,
+                               caption='Это animation 1',
+                               reply_markup=markup)
+
+
+# Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
+@router.callback_query(F.data.in_(['animation']))
+async def process_button_press(callback: CallbackQuery, bot: bot):
+    markup = create_inline_kb(2, 'animation')
+    try:
+        await bot.edit_message_media(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            media=InputMediaAnimation(
+                media=g2,
+                caption='Это animation 2'),
+            reply_markup=markup)
+    except TelegramBadRequest:
+        await bot.edit_message_media(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            media=InputMediaAnimation(
+                media=g1,
+                caption='Это animation 1'),
+            reply_markup=markup)
+
+@router.message(F.text == 'dd')
+async def process_sl(message: Message):
+    markup = create_inline_kb(2, 'document')
+    await message.answer_document(document=d1,
+                               caption='Это document 1',
+                               reply_markup=markup)
+
+
+# Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
+@router.callback_query(F.data.in_(['document']))
+async def process_button_press(callback: CallbackQuery, bot: bot):
+    markup = create_inline_kb(2, 'document')
+    try:
+        await bot.edit_message_media(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            media=InputMediaDocument(
+                media=d2,
+                caption='Это document 2'),
+            reply_markup=markup)
+    except TelegramBadRequest:
+        await bot.edit_message_media(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            media=InputMediaDocument(
+                media=d1,
+                caption='Это document 1'),
             reply_markup=markup)
