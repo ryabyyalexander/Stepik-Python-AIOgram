7ace2671571dc81192d4c5793dd2b56c06a48776
Oleksandr Riabyi <ryabyy.alexander@gmail.com>
2023-11-05 23:46:22 +0200

    refactoring

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ import asyncio
 
 # Функция конфигурирования и запуска бота
 async def main():
-    from data import dp, bot, on_startup
-    from handlers import is_admin, echo, user_block_bot, slide_media, close_bot_menu, id_media
-    from keyboards import set_main_menu_book
+    from data import dp, bot
+    from handlers import is_admin, echo, user_block_bot #, slide_media, close_bot_menu, id_media
+    # from keyboards import set_main_menu_book
 
-    dp.startup.register(set_main_menu_book)
-    dp.startup.register(on_startup)
+    # dp.startup.register(set_main_menu_book)
 
-    dp.include_router(router=close_bot_menu.router)
+    # dp.include_router(router=close_bot_menu.router)
     dp.include_router(router=is_admin.router)
     dp.include_router(router=user_block_bot.router)
-    dp.include_router(router=id_media.router)
-    dp.include_router(router=slide_media.router)
     dp.include_router(router=echo.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
