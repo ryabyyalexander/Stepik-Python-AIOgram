204f920e96d4e3baa6a7ed7b8351c4df116e0023
Oleksandr Riabyi <ryabyy.alexander@gmail.com>
2023-11-06 06:16:05 +0200

    FSM

--- data/loader.py
+++ data/loader.py
@@ -2,9 +2,11 @@ from aiogram import Bot, Dispatcher
 
 import logging
 
+from aiogram.fsm.storage.memory import MemoryStorage
+
 from .config import Config, load_config
 
-
+storage = MemoryStorage()
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
 
@@ -19,4 +21,11 @@ logger.info('Starting bot')
 
 config: Config = load_config()
 bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
-dp = Dispatcher()
+dp = Dispatcher(storage=storage)
+
+async def on_startup(_):
+    pass
+
+
+async def on_shutdown(_):
+    await storage.close()
