8527ae1ae93bdcfbbf25dbb6bd1eb25aabebed6c
Oleksandr Riabyi <ryabyy.alexander@gmail.com>
2023-11-07 11:10:35 +0200

    refactoring

--- handlers/is_admin.py
+++ handlers/is_admin.py
@@ -1,14 +1,15 @@
 from aiogram import Router, F
 from aiogram.types import Message
 
+from data import BUTTONS
 from filters import IsAdmin
-from keyboards import special_button
+from keyboards import ikb
 
 router = Router()
 
 
 @router.message(F.text == '/admin', IsAdmin)
 async def func(message: Message):
-    # keyboard = create_inline_kb(4, last_btn='Последняя кнопка', **BUTTONS)
-    await message.answer('is admin', reply_markup=special_button)
+    keyboard = ikb(4, last_btn='Последняя кнопка', **BUTTONS)
+    await message.answer('is admin', reply_markup=keyboard)
     await message.delete()
--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import asyncio
-
-from handlers import id_media, edit_media, book
-
-
-# Функция конфигурирования и запуска бота
-async def main():
+if __name__ == "__main__":
     from data import dp, bot
-    from handlers import is_admin, fsm_form, user_block_bot, close_bot_menu
+    from handlers import is_admin, fsm_form, user_block_bot, close_bot_menu, id_media, edit_media, book
     from keyboards import set_main_menu
 
     dp.startup.register(set_main_menu)
@@ -19,9 +13,4 @@ async def main():
     dp.include_router(router=id_media.router)
     dp.include_router(router=edit_media.router)
 
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    dp.run_polling(bot)
